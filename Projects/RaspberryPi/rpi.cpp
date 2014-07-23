e8f661e51a7c2aab94f79e2329ea153f8c9d169f
guoxiangyang <guoxiangyang@gmail.com>
2014-07-23 13:46:36 +0800

    replace TAB with 4 space

--- Projects/RaspberryPi/rpi.cpp
+++ Projects/RaspberryPi/rpi.cpp
@@ -40,133 +40,133 @@ char* get_token_rest(char* buf) {
 
 void Clear_String_Buffers()
 {
-	memset(Package_Type,0,sizeof(Package_Type));
-	memset(Egg_UUID,0,sizeof(Egg_UUID));
-	memset(Timestamp,0,sizeof(Timestamp));
-	memset(Egg_Acc_Data,0,sizeof(Egg_Acc_Data));
-	memset(Egg_Gyro_Data,0,sizeof(Egg_Gyro_Data));
-	memset(Temp_Data,0,sizeof(Temp_Data));
-	memset(Humi_Data,0,sizeof(Humi_Data));
-	memset(Weather_Light_Data,0,sizeof(Weather_Light_Data));
-	memset(Weather_Pressure_Data,0,sizeof(Weather_Pressure_Data));
+    memset(Package_Type,0,sizeof(Package_Type));
+    memset(Egg_UUID,0,sizeof(Egg_UUID));
+    memset(Timestamp,0,sizeof(Timestamp));
+    memset(Egg_Acc_Data,0,sizeof(Egg_Acc_Data));
+    memset(Egg_Gyro_Data,0,sizeof(Egg_Gyro_Data));
+    memset(Temp_Data,0,sizeof(Temp_Data));
+    memset(Humi_Data,0,sizeof(Humi_Data));
+    memset(Weather_Light_Data,0,sizeof(Weather_Light_Data));
+    memset(Weather_Pressure_Data,0,sizeof(Weather_Pressure_Data));
 }
 
 void MySQL_Write(int type)
 {
-	ostringstream MySQL_Command;
-	switch(type)
-	{
-		case 1:  //Egg MPU data
-		MySQL_Command<<"INSERT INTO `Egg_MPU`.`"<<Egg_UUID<<"` (`Time`, `Acc-x`, `Acc-y`, `Acc-z`, `Gyro-x`, `Gyro-y`, `Gyro-z`) VALUES (FROM_UNIXTIME('"<<Timestamp<<"'),"<<Egg_Acc_Data<<","<<Egg_Gyro_Data<<");";
-		break;
-		
-		case 2:  //Egg TMP data
-		MySQL_Command<<"INSERT INTO `Egg_TMP`.`"<<Egg_UUID<<"` (`Time`, `Temp-01`, `Temp-02`, `Temp-03`, `Temp-04`, `Temp-05`, `Temp-06`, `Temp-07`, `Temp-08`, `Temp-09`, `Temp-10`, `Temp-11`, `Temp-12`, `Temp-13`, `Temp-14`) VALUES (FROM_UNIXTIME('"<<Timestamp<<"'),"<<Temp_Data<<");";
-		break;
-		
-		case 3:  //Egg HUM data
-		MySQL_Command<<"INSERT INTO `Egg_HUM`.`"<<Egg_UUID<<"` (`Time`, `Humidity`) VALUES (FROM_UNIXTIME('"<<Timestamp<<"'),"<<Humi_Data<<");";
-		break;
-		
-		case 4:
-		MySQL_Command<<"INSERT INTO `Weather`.`EEF0` (`Time`, `Humidity`, `Light`, `Pressure`, `Temperature`) VALUES (FROM_UNIXTIME('"<<Timestamp<<"'),"<<Humi_Data<<","<<Weather_Light_Data<<","<<Weather_Pressure_Data<<","<<Temp_Data<<");";
-		break;
-		
-		default:
-		cout<<"Invalid SQL_Type."<<endl;
-		break;
-	}
-	cout << MySQL_Command.str()<<endl;
-	my.executeSQL(MySQL_Command.str());
+    ostringstream MySQL_Command;
+    switch(type)
+    {
+        case 1:  //Egg MPU data
+        MySQL_Command<<"INSERT INTO `Egg_MPU`.`"<<Egg_UUID<<"` (`Time`, `Acc-x`, `Acc-y`, `Acc-z`, `Gyro-x`, `Gyro-y`, `Gyro-z`) VALUES (FROM_UNIXTIME('"<<Timestamp<<"'),"<<Egg_Acc_Data<<","<<Egg_Gyro_Data<<");";
+        break;
+        
+        case 2:  //Egg TMP data
+        MySQL_Command<<"INSERT INTO `Egg_TMP`.`"<<Egg_UUID<<"` (`Time`, `Temp-01`, `Temp-02`, `Temp-03`, `Temp-04`, `Temp-05`, `Temp-06`, `Temp-07`, `Temp-08`, `Temp-09`, `Temp-10`, `Temp-11`, `Temp-12`, `Temp-13`, `Temp-14`) VALUES (FROM_UNIXTIME('"<<Timestamp<<"'),"<<Temp_Data<<");";
+        break;
+        
+        case 3:  //Egg HUM data
+        MySQL_Command<<"INSERT INTO `Egg_HUM`.`"<<Egg_UUID<<"` (`Time`, `Humidity`) VALUES (FROM_UNIXTIME('"<<Timestamp<<"'),"<<Humi_Data<<");";
+        break;
+        
+        case 4:
+        MySQL_Command<<"INSERT INTO `Weather`.`EEF0` (`Time`, `Humidity`, `Light`, `Pressure`, `Temperature`) VALUES (FROM_UNIXTIME('"<<Timestamp<<"'),"<<Humi_Data<<","<<Weather_Light_Data<<","<<Weather_Pressure_Data<<","<<Temp_Data<<");";
+        break;
+        
+        default:
+        cout<<"Invalid SQL_Type."<<endl;
+        break;
+    }
+    cout << MySQL_Command.str()<<endl;
+    my.executeSQL(MySQL_Command.str());
 }
 
 void Data_Parse(char* data)
 {
-	strcpy(Package_Type, (data = get_token(data, ';')));
-	if(strcmp(Package_Type, "Egg") == 0)
-	{
-		cout<<"Data from egg detected."<<endl;
-		strcpy(Egg_UUID, (data = get_next_token(data, ';')));
-		strcpy(Timestamp, (data = get_next_token(data, ';')));
-		strcpy(Egg_Acc_Data, (data = get_next_token(data, ';')));
-		strcpy(Egg_Gyro_Data, (data = get_next_token(data, ';')));
-		strcpy(Temp_Data, (data = get_next_token(data, ';')));
-		strcpy(Humi_Data, (data = get_token_rest(data)));
-		if((strlen(Egg_UUID) > 0) && (strlen(Timestamp) > 0))
-		{
-			if((strlen(Egg_Acc_Data) > 0) && (strlen(Egg_Gyro_Data) > 0)){
-				cout<<"Egg_MPU data detected."<<endl;
-				MySQL_Write(1);
-			}
-			if(strlen(Temp_Data) > 0){
-				cout<<"Egg_TMP data detected."<<endl;
-				MySQL_Write(2);
-			}
-			if(strlen(Humi_Data) > 0){
-				cout<<"Egg_HUM data detected."<<endl;
-				MySQL_Write(3);
-			} 
-		}
-		else {
-		cout<<"UUID or timestamp is empty!"<<endl;
-		}
-	}
-	else if(strcmp(Package_Type, "Weather") == 0)
-	{
-		cout<<"Data from weather station detected."<<endl;
-		strcpy(Timestamp, (data = get_next_token(data, ';')));
-		strcpy(Temp_Data, (data = get_next_token(data, ';')));
-		strcpy(Humi_Data, (data = get_next_token(data, ';')));
-		strcpy(Weather_Light_Data, (data = get_next_token(data, ';')));
-		strcpy(Weather_Pressure_Data, (data = get_token_rest(data)));
-		if(strlen(Timestamp) > 0)
-		{
-			if((strlen(Temp_Data) > 0) && (strlen(Humi_Data) > 0) && (strlen(Weather_Light_Data) > 0) && (strlen(Weather_Pressure_Data) > 0))
-			{
-				MySQL_Write(4);
-			}
-			else {
-				cout<<"Data format illegal."<<endl;
-			}
-		}
-		else {
-			cout<<"Timestamp is empty."<<endl;
-		}
-	}
-	else {
-		cout<<"Invalid package type."<<endl;
-	}
-	
-	#if DEBUG
-	cout<<"Package Type:"<<Package_Type<<endl;
-	cout<<"UUID:"<<Egg_UUID<<endl;
-	cout<<"Time Stamp:"<<Timestamp<<endl;
-	cout<<"Acceleration Data:"<<Egg_Acc_Data<<endl;
-	cout<<"Gyroscope Data:"<<Egg_Gyro_Data<<endl;
-	cout<<"Temperature Data:"<<Temp_Data<<endl;
-	cout<<"Humidity Data:"<<Humi_Data<<endl;
-	#endif
-	Clear_String_Buffers();
+    strcpy(Package_Type, (data = get_token(data, ';')));
+    if(strcmp(Package_Type, "Egg") == 0)
+    {
+        cout<<"Data from egg detected."<<endl;
+        strcpy(Egg_UUID, (data = get_next_token(data, ';')));
+        strcpy(Timestamp, (data = get_next_token(data, ';')));
+        strcpy(Egg_Acc_Data, (data = get_next_token(data, ';')));
+        strcpy(Egg_Gyro_Data, (data = get_next_token(data, ';')));
+        strcpy(Temp_Data, (data = get_next_token(data, ';')));
+        strcpy(Humi_Data, (data = get_token_rest(data)));
+        if((strlen(Egg_UUID) > 0) && (strlen(Timestamp) > 0))
+        {
+            if((strlen(Egg_Acc_Data) > 0) && (strlen(Egg_Gyro_Data) > 0)){
+                cout<<"Egg_MPU data detected."<<endl;
+                MySQL_Write(1);
+            }
+            if(strlen(Temp_Data) > 0){
+                cout<<"Egg_TMP data detected."<<endl;
+                MySQL_Write(2);
+            }
+            if(strlen(Humi_Data) > 0){
+                cout<<"Egg_HUM data detected."<<endl;
+                MySQL_Write(3);
+            } 
+        }
+        else {
+        cout<<"UUID or timestamp is empty!"<<endl;
+        }
+    }
+    else if(strcmp(Package_Type, "Weather") == 0)
+    {
+        cout<<"Data from weather station detected."<<endl;
+        strcpy(Timestamp, (data = get_next_token(data, ';')));
+        strcpy(Temp_Data, (data = get_next_token(data, ';')));
+        strcpy(Humi_Data, (data = get_next_token(data, ';')));
+        strcpy(Weather_Light_Data, (data = get_next_token(data, ';')));
+        strcpy(Weather_Pressure_Data, (data = get_token_rest(data)));
+        if(strlen(Timestamp) > 0)
+        {
+            if((strlen(Temp_Data) > 0) && (strlen(Humi_Data) > 0) && (strlen(Weather_Light_Data) > 0) && (strlen(Weather_Pressure_Data) > 0))
+            {
+                MySQL_Write(4);
+            }
+            else {
+                cout<<"Data format illegal."<<endl;
+            }
+        }
+        else {
+            cout<<"Timestamp is empty."<<endl;
+        }
+    }
+    else {
+        cout<<"Invalid package type."<<endl;
+    }
+    
+    #if DEBUG
+    cout<<"Package Type:"<<Package_Type<<endl;
+    cout<<"UUID:"<<Egg_UUID<<endl;
+    cout<<"Time Stamp:"<<Timestamp<<endl;
+    cout<<"Acceleration Data:"<<Egg_Acc_Data<<endl;
+    cout<<"Gyroscope Data:"<<Egg_Gyro_Data<<endl;
+    cout<<"Temperature Data:"<<Temp_Data<<endl;
+    cout<<"Humidity Data:"<<Humi_Data<<endl;
+    #endif
+    Clear_String_Buffers();
 }
 
 int main()
 {
-	cout<<"Open serial port."<<endl;
-	int fd = serialOpen("/dev/ttyAMA0",115200);
-	int datalength,dataflag;
-	char ch;
-	char rx_buf[RX_BUF_SIZE];
+    cout<<"Open serial port."<<endl;
+    int fd = serialOpen("/dev/ttyAMA0",115200);
+    int datalength,dataflag;
+    char ch;
+    char rx_buf[RX_BUF_SIZE];
     int  rx_buf_len;
-	my.initDB("localhost" , "root", "root" , "" );
-	while(1)
-	{
-		cout<<"FD:"<<fd<<". "<<"Start waiting."<<endl;
-		if (serialDataAvail(fd)) {;
-			ch = serialGetchar(fd);
-			cout<<ch;
-			rx_buf[rx_buf_len] = ch;
-			if ((rx_buf_len > 2) && (ch == 0x0A && buf[rx_buf_len - 1] == 0x0D)) {
-				buf[rx_buf_len - 1] = 0;
+    my.initDB("localhost" , "root", "root" , "" );
+    while(1)
+    {
+        cout<<"FD:"<<fd<<". "<<"Start waiting."<<endl;
+        if (serialDataAvail(fd)) {;
+            ch = serialGetchar(fd);
+            cout<<ch;
+            rx_buf[rx_buf_len] = ch;
+            if ((rx_buf_len > 2) && (ch == 0x0A && buf[rx_buf_len - 1] == 0x0D)) {
+                buf[rx_buf_len - 1] = 0;
                 cout<<"Data:"<<buf<<endl;
                 char *data = buf;
                 Data_Parse(data);
@@ -178,9 +178,9 @@ int main()
                     rx_buf_len++;
                 }
             }
-			usleep(80);
-		}
-	}
-	serialClose(fd);
-	return 0;
+            usleep(80);
+        }
+    }
+    serialClose(fd);
+    return 0;
 }
